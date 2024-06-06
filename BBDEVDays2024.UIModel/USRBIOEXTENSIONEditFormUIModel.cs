c6e2dd016b5f59a78a3f675fd10d9db5d9c5daf6
wekivashep <shep@wekivasoftware.com>
2024-06-06 10:45:41 -0400

    Catalog and uimodel for BBDEVDays demo

---
+++ BBDEVDays2024.UIModel/USRBIOEXTENSIONEditFormUIModel.cs
@@ -0,0 +1,23 @@
+namespace BBDEVDays2024.UIModel
+{
+
+	public partial class USRBIOEXTENSIONEditFormUIModel
+	{
+
+		private void USRBIOEXTENSIONEditFormUIModel_Loaded(object sender, Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs e)
+		{
+
+		}
+
+#region "Event handlers"
+
+		partial void OnCreated()
+		{
+			this.Loaded += USRBIOEXTENSIONEditFormUIModel_Loaded;
+		}
+
+#endregion
+
+	}
+
+}
